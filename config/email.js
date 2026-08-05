3360883f75e414f9336a7483a5c622238902ff35
jay-bee270 <jubril@gmail.com>
2026-08-05 14:43:50 +0100

    Forced IPv4cfor gmail SMTP on render

--- config/email.js
+++ config/email.js
@@ -6,9 +6,16 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  // Force IPv4 to avoid ENETUNREACH on Render
+  host: 'smtp.gmail.com',
+  family: 4,
   // Timeouts to prevent hanging
-  connectionTimeout: 5000,   // 5 seconds
-  greetingTimeout: 5000,     // 5 seconds
+  connectionTimeout: 5000,
+  greetingTimeout: 5000,
+  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = transporter;
